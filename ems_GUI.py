1eb97adbf10339a72396376cb8bc88badc2d18aa
Akaayush14 <akaayush40@gmail.com>
2025-02-22 18:54:57 +0545

    Changed grid by place method, and adjusted placing of picture

--- ems_GUI.py
+++ ems_GUI.py
@@ -7,142 +7,146 @@ window = CTk()
 #Fullscreen window:
 def maximize_window():
     window.state('zoomed')
-window.after(10,maximize_window)
+window.after(10, maximize_window)
 
 #Background colour of window:                               
-window.configure(fg_color = "cadet blue")
+window.configure(fg_color="cadet blue")
 
-#window title:
+#Window title:
 window.title("Employee Management System")
 
 #GUI image:
-image_1 = CTkImage(Image.open("1.png"), size = (920,200))
-image_label_1 = CTkLabel(window, image = image_1, text = " ", fg_color = "white") 
-image_label_1.grid(row=0, column=0)
+image_1 = CTkImage(Image.open("1.png"), size=(920, 200))
+image_label_1 = CTkLabel(window, image=image_1, text=" ", fg_color="white", width=920, height=200)  # Set width and height here
+image_label_1.place(x=0, y=0, anchor="nw") 
 
-image_2 = CTkImage(Image.open("2.png"), size = (920,200))
-image_label_2 = CTkLabel(window, image = image_2, text = " ", fg_color = "white") 
-image_label_2.grid(row=0, column=1)
+image_2 = CTkImage(Image.open("2.png"), size=(920, 200))
+image_label_2 = CTkLabel(window, image=image_2, text=" ", fg_color="white", width=920, height=200)  # Set width and height here
+image_label_2.place(relx=1.0, y=0, anchor="ne")
 
-#Left frame(frame-1):
-left_frame = CTkFrame(window, fg_color = "cadet blue")
-left_frame.grid(row=1, column=0)
+################################################################Left frame(frame-1)######################################################################################
+left_frame = CTkFrame(window, fg_color="cadet blue", width=940, height=600)
+left_frame.place(x=10, y=210)  # Positioned below image_label_1
 
-#Label and entry inside left frame: 
-id_label = CTkLabel(left_frame, text = 'Id', fg_color = 'cadet blue', text_color = 'white', font = ('aerial', 18, 'bold'))
-id_label.grid(row=0, column=0, padx=20,  pady=15, sticky='w')
+#Labels: 
+id_label = CTkLabel(left_frame, text='Id', fg_color='cadet blue', text_color='white', font=('aerial', 20, 'bold'))
+id_label.place(x=60, y=45)
 
-id_entry = CTkEntry(left_frame, font = ('helvetica', 15, 'bold'), fg_color = 'white', text_color = 'black', width = 180)
-id_entry.grid(row=0, column=1)
+name_label = CTkLabel(left_frame, text='Name', fg_color='cadet blue', text_color='white', font=('aerial', 20, 'bold'))
+name_label.place(x=60, y=140)
 
-name_label = CTkLabel(left_frame, text = 'Name', fg_color = 'cadet blue', text_color = 'white', font = ('aerial', 18, 'bold'))
-name_label.grid(row=1, column=0, padx=20, pady=15, sticky='w')
+phone_label = CTkLabel(left_frame, text='Phone', fg_color='cadet blue', text_color='white', font=('aerial', 20, 'bold'))
+phone_label.place(x=60, y=235)
 
-name_entry = CTkEntry(left_frame, font = ('helvetica', 15, 'bold'), fg_color = 'white', text_color = 'black', width = 180)
-name_entry.grid(row=1, column=1)
+role_label = CTkLabel(left_frame, text='Role', fg_color='cadet blue', text_color='white', font=('aerial', 20, 'bold'))
+role_label.place(x=60, y=330)
 
-phone_label = CTkLabel(left_frame, text = 'Id', fg_color = 'cadet blue', text_color = 'white', font = ('aerial', 18, 'bold'))
-phone_label.grid(row=2, column=0, padx=20, pady=15, sticky='w')
+gender_label = CTkLabel(left_frame, text='Gender', fg_color='cadet blue', text_color='white', font=('aerial', 20, 'bold'))
+gender_label.place(x=60, y=425)
 
-phone_entry = CTkEntry(left_frame, font = ('helvetica', 15, 'bold'), fg_color = 'white', text_color = 'black', width = 180)
-phone_entry.grid(row=2, column=1)
+salary_label = CTkLabel(left_frame, text='Salary', fg_color='cadet blue', text_color='white', font=('aerial', 20, 'bold'))
+salary_label.place(x=60, y=520)
 
-role_label = CTkLabel(left_frame, text = 'Role', fg_color = 'cadet blue', text_color = 'white', font = ('aerial', 18, 'bold'))
-role_label.grid(row=3, column=0, padx=20, pady=15, sticky='w')
+#Entries and comboboxes:
+id_entry = CTkEntry(left_frame, font=('helvetica', 18, 'bold'), fg_color='white', text_color='black', width=300, height=45)
+id_entry.place(x=230, y=40)
 
-role_options = ['Business Analyst', 'Cloud Architect', 'Relational Manager', 'Data Scientist', 'DevOps Engineer', 'IT consultant', 'Network Engineer', 'Software Engineer', 'UX/UI Designer', 'Web Developer',]
-role_box = CTkComboBox(left_frame, values = role_options, font = ('helvetica', 15, 'bold'), fg_color = 'white', text_color = 'black', width = 180, state = 'readonly')
-role_box.grid(row=3, column=1)
+name_entry = CTkEntry(left_frame, font=('helvetica', 18, 'bold'), fg_color='white', text_color='black', width=300, height=45)
+name_entry.place(x=230, y=135)
+
+phone_entry = CTkEntry(left_frame, font=('helvetica', 18, 'bold'), fg_color='white', text_color='black', width=300, height=45)
+phone_entry.place(x=230, y=230)
+
+role_options = ['Business Analyst', 'Cloud Architect', 'Relational Manager', 'Data Scientist', 'DevOps Engineer', 'IT consultant', 'Network Engineer', 'Software Engineer', 'UX/UI Designer', 'Web Developer']
+role_box = CTkComboBox(left_frame, values=role_options, font=('helvetica', 18, 'bold'), fg_color='white', text_color='black', state='readonly', width=300, height=45)
+role_box.place(x=230, y=325)
 role_box.set(role_options[0])
 
-gender_label = CTkLabel(left_frame, text = 'Gender', fg_color = 'cadet blue', text_color = 'white', font = ('aerial', 18, 'bold'))
-gender_label.grid(row=4, column=0, padx=20, pady=15, sticky='w')
-
 gender_options = ['Male', 'Female', 'Others']
-gender_box = CTkComboBox(left_frame, values = gender_options, font = ('helvetica', 15, 'bold'), fg_color = 'white', text_color = 'black', width = 180, state = 'readonly')
-gender_box.grid(row=4, column=1)
+gender_box = CTkComboBox(left_frame, values=gender_options, font=('helvetica', 18, 'bold'), fg_color='white', text_color='black', state='readonly', width=300, height=45)
+gender_box.place(x=230, y=420)
 gender_box.set(gender_options[0])
 
-salary_label = CTkLabel(left_frame, text = 'Salary', fg_color = 'cadet blue', text_color = 'white', font = ('aerial', 18, 'bold'))
-salary_label.grid(row=5, column=0, padx=20, pady=15, sticky='w')
+salary_entry = CTkEntry(left_frame, font=('helvetica', 18, 'bold'), fg_color='white', text_color='black', width=300, height=45)
+salary_entry.place(x=230, y=515)
 
-salary_entry = CTkEntry(left_frame, font = ('helvetica', 15, 'bold'), fg_color = 'white', text_color = 'black', width = 180)
-salary_entry.grid(row=5, column=1)
+#####################################################################Right frame(frame-2)################################################################################
+right_frame = CTkFrame(window, fg_color="light grey", width=1090, height=700)
+right_frame.place(x=590, y=210)  # Positioned below image_label_2
 
-#Right frame(frame-2):
-right_frame = CTkFrame(window, fg_color = "light grey")
-right_frame.grid(row=1, column=1)
-
-#Adding label and entry inside right frame:
+#Entries and comboboxes:
 search_options = ['Id', 'Name', 'Phone', 'Role', 'Gender', 'Salary']
-search_box = CTkComboBox(right_frame, values = search_options, font = ('helvetica', 15, 'bold'), fg_color = 'white', text_color = 'black', state = 'readonly')
-search_box.grid(row=0, column=0)
+search_box = CTkComboBox(right_frame, values=search_options, font=('helvetica', 15, 'bold'), fg_color='white', text_color='black', state='readonly', width=200)
+search_box.place(x=30, y=10)
 search_box.set('Search By')
 
-search_Entry = CTkEntry(right_frame, font = ('helvetica', 15, 'bold'), fg_color = 'white', text_color = 'black')
-search_Entry.grid(row=0, column=1)
+search_Entry = CTkEntry(right_frame, font=('helvetica', 15, 'bold'), fg_color='white', text_color='black', width=200)
+search_Entry.place(x=300, y=10)
 
-#Creating Buttons in right frame portion:
-search_button = CTkButton(right_frame, text = "Search", width = 100)
-search_button.grid(row=0, column=2)
+#Buttons:
+search_button = CTkButton(right_frame, text="Search", width=200)
+search_button.place(x=575, y=10)
 
-showall_button = CTkButton(right_frame, text = "Show All", width = 100)
-showall_button.grid(row=0, column=3, pady=5) #padding in one button, that same padding will be applied to all others.
+showall_button = CTkButton(right_frame, text="Show All", width=200)
+showall_button.place(x=850, y=10)
 
-#Accessing scrollbar class and keeping it in right frame:
-scrollbar = ttk.Scrollbar(right_frame, orient = VERTICAL)
-scrollbar.grid(row=1, column=4, sticky='ns')
+#Accessing scrollbar class:
+scrollbar = ttk.Scrollbar(right_frame, orient=VERTICAL)
+scrollbar.place(x=1605, y=71)
 
 #Adding a tree:
-tree = ttk.Treeview(right_frame, height = 13)
-tree.grid(row=1, column = 0, columnspan=4)
+tree = ttk.Treeview(right_frame, height=13, yscrollcommand=scrollbar.set)
+tree.place(x=4, y=70, width=1600, height=871)
 
-#columns in tree:
+# Configure the scrollbar to scroll the Treeview
+scrollbar.config(command=tree.yview)  # This links the scrollbar to the vertical scroll of the treeview
+
+#Columns in tree:
 tree['columns'] = ('Id', 'Name', 'Phone', 'Role', 'Gender', 'Salary')
 
 #Headings in tree:
-tree.heading('Id', text = 'Id')
-tree.heading('Name', text = 'Name')
-tree.heading('Phone', text = 'Phone')
-tree.heading('Role', text = 'Role')
-tree.heading('Gender', text = 'Gender')
-tree.heading('Salary', text = 'Salary')
+tree.heading('Id', text='Id')
+tree.heading('Name', text='Name')
+tree.heading('Phone', text='Phone')
+tree.heading('Role', text='Role')
+tree.heading('Gender', text='Gender')
+tree.heading('Salary', text='Salary')
 
 #Process to remove extra columns:
-tree.config(show = 'headings')
+tree.config(show='headings')
 
-#changing widths of columns:
-tree.column('Id', width = 140)
-tree.column('Name', width = 160)
-tree.column('Phone', width = 160)
-tree.column('Role', width = 200)
-tree.column('Gender', width = 140)
-tree.column('Salary', width = 140)
+#Changing widths of columns:
+tree.column('Id', width=140)
+tree.column('Name', width=160)
+tree.column('Phone', width=160)
+tree.column('Role', width=200)
+tree.column('Gender', width=140)
+tree.column('Salary', width=140)
 
 #Styling the tree:
 style = ttk.Style()
 
-#changing font of treeview heading:
-style.configure('Treeview.Heading', font = ("aerial", 18, "bold"))
+#Changing font of treeview heading:
+style.configure('Treeview.Heading', font=("aerial", 18, "bold"))
 
-#Button frame(frame-3):
-button_frame = CTkFrame(window, fg_color = 'cadet blue')
-button_frame.grid(row=2, column=0, columnspan=2)
+###########################################################################Below frame(Frame-3)##########################################################################
+button_frame = CTkFrame(window, fg_color="cadet blue", width=1870, height=80)
+button_frame.place(x=10, y=840)  # Positioned at the bottom
 
-#Buttons in frame-3:
-new_button = CTkButton(button_frame, text = 'New Employee', font = ('aerial', 15, 'bold'), width = 160, corner_radius = 15)
-new_button.grid(row=0, column=0, pady=5)
+#Buttons:
+new_button = CTkButton(button_frame, text='New Employee', font=('aerial', 15, 'bold'), width=200, height=35, corner_radius=15)
+new_button.place(x=50, y=10)
 
-add_button = CTkButton(button_frame, text = 'Add Employee', font = ('aerial', 15, 'bold'), width = 160, corner_radius = 15)
-add_button.grid(row=0, column=1, padx=5, pady=5)
+add_button = CTkButton(button_frame, text='Add Employee', font=('aerial', 15, 'bold'), width=200, height=35, corner_radius=15)
+add_button.place(x=400, y=10)
 
-update_button = CTkButton(button_frame, text = 'Update Employee', font = ('aerial', 15, 'bold'), width = 160, corner_radius = 15)
-update_button.grid(row=0, column=2, padx=5, pady=5)
+update_button = CTkButton(button_frame, text='Update Employee', font=('aerial', 15, 'bold'), width=200, height=35, corner_radius=15)
+update_button.place(x=750, y=10)
 
-delete_button = CTkButton(button_frame, text = 'Delete Employee', font = ('aerial', 15, 'bold'), width = 160, corner_radius = 15)
-delete_button.grid(row=0, column=3, padx=5, pady=5)
+delete_button = CTkButton(button_frame, text='Delete Employee', font=('aerial', 15, 'bold'), width=200, height=35, corner_radius=15)
+delete_button.place(x=1100, y=10)
 
-deleteall_button = CTkButton(button_frame, text = 'Delete All', font = ('aerial', 15, 'bold'), width = 160, corner_radius = 15)
-deleteall_button.grid(row=0, column=4, padx=5, pady=5)
+deleteall_button = CTkButton(button_frame, text='Delete All', font=('aerial', 15, 'bold'), width=200, height=35, corner_radius=15)
+deleteall_button.place(x=1420, y=10)
 
 window.mainloop()
