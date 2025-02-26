41bf416833d955691585bedd56c0f02b9bea7a7e
Akaayush14 <akaayush40@gmail.com>
2025-02-26 13:09:51 +0545

    Rremoved pycache and temprunner file

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-#calling the on_closing fuction and here the predefined text will tell the tkinter to close the window.
-root.protocol("WM_DELETE_WINDOW", on_closing)
-
-#To show a confirmatory window closing message box:
-root.protocol("on_closing") 
